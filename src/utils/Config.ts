20339190996c0339e322baa3253ef2e4a3f6aa83
Artem <arlaptiev@gmail.com>
2021-02-18 16:35:25 +0200

    meta

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,14 +1,15 @@
 export const Config = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'PONY.STUDIO',
+  title: 'UX UI Design Studio in London - Pony',
+  description:
+    'Pony Studio is a UX UI design agency based in London. We design digital products and brand identities for startups and scale-ups. Clients backed by Techstars, YC, Seedcamp etc.',
   locale: 'en',
   content_type: 'website',
   preview_image: {
-    url: '/assets/img/nextjs-starter-banner.png',
+    url: '/assets/img/meta-lg.png',
     width: 1200,
     height: 600,
-    alt: 'Start your next project with this boilerplate banner',
+    alt: 'We create brands and products',
   },
   theme_primary_color: '#000000',
 };
